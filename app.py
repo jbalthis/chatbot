9dfa2b0ff59bf528b6129c9ea765b4c9012ca4ba
jbalthis <balthis.jason@gmail.com>
2023-06-09 04:26:53 +0000

    layout: index template added

--- app.py
+++ app.py
@@ -6,3 +6,21 @@ from dotenv import load_dotenv
 
 
 # https://platform.openai.com/account/api-keys
+load_dotenv()
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+# print(openai.api_key)
+
+# Setup Flask app
+app = Flask(__name__)
+
+
+# Define the home page route
+@app.route("/")
+def Home():
+  return render_template("index.html")
+
+# Define Chatbot route
+@app.route("/chatbot", methods=["POST"])
+def chatbot():
+  pass
